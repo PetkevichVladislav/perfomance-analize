babc5f0bf39860e1b6bb2b26f6e5134a9a835cb9
Uladzislau <Uladzislau@AZURE>
2023-12-04 00:16:00 +0100

    .

--- HttpTrigger1/index.js
+++ HttpTrigger1/index.js
@@ -1,4 +1,4 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require("puppeteer");
 const lighthouse = require('lighthouse');
 const OpenAI = require('openai');
 const axios = require('axios');
@@ -7,24 +7,26 @@ const OPEN_AI_KEY = process.env["OPEN_AI_KEY"];
 const LOGIC_APP_URL = process.env["LOGIC_APP_URL"];
 
 module.exports = async function (context, req) {
-	const url = req.query.url || (req.body && req.body.url);
-	const email = req.query.email || (req.body && req.body.email);
+	context.log(`OPEN_AI_KEY value is not null: ${!(OPEN_AI_KEY === undefined || OPEN_AI_KEY === null)}`);
+	context.log(`LOGIC_APP_URL value is not null: ${!(LOGIC_APP_URL === undefined || LOGIC_APP_URL === null)}`);
+	var url = req.query.url || (req.body && req.body.url);
+	var email = req.query.email || (req.body && req.body.email);
 	context.log(`Analyzing URL ${url}`);
 
 	try {
-		var browser = await getBrowser();
-		var report = await getLightHouseReport(browser, url);
-		var prompt = createPromtFromReports(report, url);
-		var recomendations = await generateOpenAIRecomendation(prompt);
-		sendEmalWithReport(email, recomendations);
+		var browser = await getBrowser(context);
+		var report = await getLightHouseReport(context, browser, url);
+		var prompt = createPromtFromReports(context, report, url);
+		var recomendations = await generateOpenAIRecomendation(context, prompt);
+		sendEmalWithReport(context, email, recomendations);
 		context.res = {
 			status: 200,
 			body: null,
 		};
 	}
 	catch (error) {
-		console.error('Error during perfomance analise run:', error.message);
-		
+		context.error('Error during perfomance analise run:', error.message);
+
 		context.res = {
 			status: 500,
 			body: error,
@@ -36,22 +38,27 @@ module.exports = async function (context, req) {
 	}
 };
 
-async function getBrowser() {
-	console.log("Start to create browser");
-	var browser = await puppeteer.launch({
-		headless: true,
-		timeout: 0,
-		args: ['--no-sandbox']
-	});
+async function getBrowser(context) {
+	try {
 
-	console.log("Browser created");
+		context.log("Start to create browser");
+		var browser = await puppeteer.launch({
+			headless: true,
+			args: ['--no-sandbox']
+		});
 
-	return browser;
+		context.log("Browser is created");
+
+		return browser;
+	} catch (error) {
+		context.error('Error during creating browser:', error.message);
+		throw error;
+	}
 }
 
-async function getLightHouseReport(browser, url) {
+async function getLightHouseReport(context, browser, url) {
 	try {
-		console.log("Start to generate lighthouse report");
+		context.log("Start to generate lighthouse report");
 		const { lhr } = await lighthouse(url, {
 			port: new URL(browser.wsEndpoint()).port,
 			output: 'json',
@@ -59,7 +66,7 @@ async function getLightHouseReport(browser, url) {
 			//onlyCategories: [ 'seo', 'performance', 'accessibility', 'best-practices' ]
 		});
 
-		console.log("Lighthouse report generated:" + lhr);
+		context.log("Lighthouse report generated:" + lhr);
 		const formattedReport =
 		{
 			performanceScore: lhr.categories.performance.score * 100,
@@ -70,62 +77,79 @@ async function getLightHouseReport(browser, url) {
 			bestpractices: lhr.categories['best-practices'].score
 			//audits: lhr.audits,
 		};
-		console.log("Lighthouse report formated" + formattedReport);
+		context.log("Lighthouse report formated" + formattedReport);
 
 		return formattedReport;
 	} catch (error) {
-		console.error('Error during Lighthouse run:', error.message);
+		context.error('Error during Lighthouse run:', error.message);
 		throw error;
 	}
 }
 
-async function generateOpenAIRecomendation(prompt) {
-	console.log("Start to create open ai recomendations:" + prompt);
-	const openai = new OpenAI({
-		apiKey: OPEN_AI_KEY,
-	});
-	var recomendations = await sendPromptToOpenAi(openai, prompt);
-	console.log("Open ai recomendations created:" + recomendations);
-
-	return recomendations;
-}
-
-async function sendPromptToOpenAi(openAiClient, prompt) {
+async function generateOpenAIRecomendation(context, prompt) {
 	try {
-		const response = await openAiClient.chat.completions.create({
-			messages: [{ role: "system", content: prompt }],
-    		model: "gpt-3.5-turbo",
+		context.log("Start to create open ai recomendations:" + prompt);
+		const openai = new OpenAI({
+			apiKey: OPEN_AI_KEY,
 		});
+		var recomendations = await sendPromptToOpenAi(context, openai, prompt);
+		context.log("Open ai recomendations created:" + recomendations);
 
-		return response.choices[0].message.content;
+		return recomendations;
 	} catch (error) {
-		console.log(error);
-		throw "Enable to proccess results with AI.";
+		context.error('Error during creating open ai recomendations:', error.message);
+		throw error;
 	}
 }
 
-function createPromtFromReports(report, url) {
-	console.log("Start to create prompt from report:" + report);
+async function sendPromptToOpenAi(context, openAiClient, prompt) {
+	try {
+		var response = await openAiClient.chat.completions.create({
+			messages: [{ role: "system", content: prompt }],
+			model: "gpt-3.5-turbo",
+		});
 
-	var rerportAJson = JSON.stringify(report);
-	var prompt = `Analise perfomance report and make list of suggestions base on the report: ${rerportAJson}. At the start of the messaage add message: Perfomance analise for web site: ${url} : `;
-
-	console.log("Prompt created:" + prompt);
-	return prompt;
+		context.log("Getting result from open ai. Recomendations:" + response);
+		return response.choices[0].message.content;
+	}
+	catch (error) {
+		context.error("Open ai returned error:" + error);
+		throw error;
+	}
 }
 
-async function sendEmalWithReport(email, body) {
-	var jsonData = {
-		email: email,
-		report: body,
-	};
-
-	console.log("Start to sending report:" + jsonData);
+function createPromtFromReports(context, report, url) {
 	try {
-		const response = await axios.post(LOGIC_APP_URL, jsonData);
-		console.log("Response from sending request:" + response.status);
+		context.log("Start to create prompt from report:" + report);
+
+		var rerportAJson = JSON.stringify(report);
+		var prompt = `Analise perfomance report and make list of suggestions base on the report: ${rerportAJson}. At the start of the messaage add message: Perfomance analise for web site: ${url} : `;
+
+		context.log("Prompt created:" + prompt);
+		return prompt;
 	} catch (error) {
-		console.log(error);
+		context.error('Error during crating prompt:', error.message);
+		throw error;
+	}
+}
+
+async function sendEmalWithReport(context, email, body) {
+	try {
+		var jsonData = {
+			email: email,
+			report: body,
+		};
+
+		context.log("Start to sending report:" + jsonData);
+		try {
+			const response = await axios.post(LOGIC_APP_URL, jsonData);
+			context.log("Response from sending request:" + response.status);
+		} catch (error) {
+			context.log(error);
+		}
+	} catch (error) {
+		context.error('Error during seding report:', error.message);
+		throw error;
 	}
 }
 
